d5269c7425f1967c570411e1acc01951eb00d281
Hanakhin <hanakhin.nounimassotte@gmail.com>
2024-11-07 00:45:31 +0100

    added scroll up , and bg on scroll down for nav

--- app/_Components/Contact.tsx
+++ app/_Components/Contact.tsx
@@ -1,28 +1,48 @@
+"use client"
+
 import {Section} from "@/app/_Components/Section";
 import {Badge} from "@/components/ui/badge";
 import {ContactCard} from "@/app/_Components/contactCard";
+import {ArrowUp} from "lucide-react";
 
 export const Contact = ()=>{
+
+    const handleScrollTop =()=>{
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    }
     return(
         <Section className="flex flex-col items-start gap-4">
             <Badge variant={'outline'}>Socials</Badge>
-            <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">Let&#39;s get in touch</h2>
+            <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">Let&#39;s get in
+                touch</h2>
             <div className="flex max-md:flex-col gap-4 w-full">
                 <ContactCard
                     image={"/image/Me.png"}
                     mediumImage={"/image/icons/linkedin.png"}
                     name={"Linkedin/hanakhin"}
-                    description={"my linkedin "}
+                    description={"Contact me"}
                     url={"https://www.linkedin.com/in/hanakhin-nouni-massotte-b6360b30b/"}
                 />
                 <ContactCard
                     image={"/image/Me.png"}
                     name={"gmail/hanakhin"}
                     mediumImage={"/image/icons/gmail.png"}
-                    description={"You have questions ? email me here"}
+                    description={"Any questions ?"}
                     url={"mailto:hanakhin.nounimassotte@gmail.com"}
                 />
             </div>
+
+            <div className={
+                "bg-primary-foreground/50 p-1 rounded-full w-max justify-center self-center flex cursor-pointer"
+            }
+                 onClick={handleScrollTop}
+            >
+                <ArrowUp className={"hover:-translate-y-0.5 transition-all"} />
+            </div>
+
         </Section>
     )
 }
--- app/_Components/Header.tsx
+++ app/_Components/Header.tsx
@@ -1,15 +1,33 @@
+"use client"
+
 import {Section} from "@/app/_Components/Section";
 import {GithubIcon} from "@/app/_Components/icons/GithubIcon";
 import Link from "next/link";
 import {cn} from "@/lib/utils";
 import {buttonVariants} from "@/components/ui/button";
 import {LinkedInIcon} from "@/app/_Components/icons/LinkedInIcon";
+import Image from "next/image";
+import {useEffect, useState} from "react";
 
 export const Header = ()=>{
+
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const handleScroll = () => {
+        const position = window.scrollY;
+        setScrollPosition(position);
+    };
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll, { passive: true });
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [scrollPosition]);
+
     return(
-        <header className={"sticky top-0 py-4"}>
-            <Section className="flex items-baseline">
-                <h1 className={"text-lg font-bold text-primary"}>Hanakhin.com</h1>
+        <header className={cn(`sticky top-0 py-4 ${scrollPosition > 50 ? "bg-primary-foreground/50 transition-all" : ""}`)}>
+            <Section className="flex items-center">
+                <Image src={"/image/icon.png"} alt={"my logo"} width={25} height={25} />
                 <div className={"flex flex-1 justify-end"}>
                     <ul className={"flex items-center gap-4"}>
                         <Link href={"https://github.com/Hanakhin"} target={"_blank"} className={cn(buttonVariants({variant: "outline"}) ,"size-8 p-2")} >
